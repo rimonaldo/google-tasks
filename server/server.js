312c74a8747ca12aff4d7f8540b0376a6bb136a0
Roni Mazor <ro6596@gmail.com>
2022-12-27 13:23:26 +0200

    basic server.js no routes.

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const app = express()
+
+app.use(cookieParser())
+app.use(express.json())
+
+
+
+app.use(express.json({ limit: '50mb',extended:true }));
+app.use(express.urlencoded({ limit: '50mb' ,extended: true, parameterLimit: 50000 }));
+
+
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+    const corsOptions = {
+        origin: ['http://127.0.0.1:5173', 'http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
+}
+
+
+
+
+// app.get('/**', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
+// })
+
+
+const port = process.env.PORT || 3030
+http.listen(port, () => {
+    logger.info('Server is running on port: ' + port)
+})
